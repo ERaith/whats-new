28d1468ca0c11b01bfdbc6f9b34eccf3be8885f3
Erik Raith <raithhtiar@gmail.com>
2020-03-16 22:26:56 -0600

    [feat] Add NewsContainer class which wrapps a news article

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import './NewsContainer.css'
+import React, { Component } from 'react';
+import './NewsContainer.css';
+import NewsArticle from '../NewsArticle/NewsArticle.js';
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+class NewsContainer extends Component {
+  render() {
+    return (
+      <article>
+        {this.props.articles.map(article => (
+          <NewsArticle key = {article.id} article = {article} articleHeadline =  {article.headline} articleDescription =  {article.description} articleImg =  {article.img} articleUrl =  {article.url}/>
+        )) }
+      </article>
+    )
+  }
+}
 
 export default NewsContainer;
