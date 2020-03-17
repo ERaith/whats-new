f861b8c56edc6e1d40e1341fab8e8d94279d79ba
Erik Raith <raithhtiar@gmail.com>
2020-03-16 22:24:08 -0600

    [feat] Add NewsContainer to render component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
 import './App.css';
+import NewsContainer from '../NewsContainer/NewsContainer.js'
 
 class App extends Component {
   constructor() {
@@ -12,9 +13,9 @@ class App extends Component {
 
   render () {
     return (
-      <div className="app">
-        YOUR CODE GOES HERE!
-      </div>
+      <section className="app">
+      <NewsContainer articles = {this.state.local}/>
+      </section>
     );
   }
 }
