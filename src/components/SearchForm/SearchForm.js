6734f233cab93f9221ec6dd74cee130d9d6a1acf
Erik Raith <raithhtiar@gmail.com>
2020-03-18 12:50:54 -0600

    [feat][wip] Add state to searchform that updates on change

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,17 +5,19 @@ class SearchForm extends Component {
   constructor() {
     super()
       this.state = {
-        title:''
+        search:''
       }
   }
   updateState = (event) => {
-
+    console.log(event.target.value)
+    const updatedState = event.target.value;
+    this.setState({search:updatedState})
   }
   render() {
     return (
       <React.Fragment>
       <label for = "search">Search</label>
-      <input id = "search"/>
+      <input id = "search" onChange = {event => this.updateState(event)}/>
       </React.Fragment>
     )
   }
