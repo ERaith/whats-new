8a2f7aa0aa3f78a7c0eebe596feb16e211c0c864
Erik Raith <raithhtiar@gmail.com>
2020-03-17 23:13:34 -0600

    [feat] add nav bar for sorting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
 import local from "../../data/local";
+import health from "../../data/health";
+import entertainment from "../../data/entertainment";
+import science from "../../data/science";
+import technology from "../../data/technology";
 import "./App.css";
 import NewsContainer from "../NewsContainer/NewsContainer.js";
+import Menu from "../Menu/Menu.js";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      local,
+      health,
+      entertainment,
+      science,
+      technology,
+      active: "local"
     };
   }
+  filterNews = filterKey => {
+    this.setState({ active: filterKey });
+  };
 
   render() {
     return (
-      <section className="app">
-        <NewsContainer articles={this.state.local} />{" "}
-      </section>
+      <>
+        <Menu filterNews={this.filterNews} />
+        <section className="app">
+          <NewsContainer articles={this.state[this.state.active]} />{" "}
+        </section>
+      </>
     );
   }
 }
