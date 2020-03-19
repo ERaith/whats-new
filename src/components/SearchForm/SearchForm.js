75d43d6b879a014851cd45cd40625fa2d23b757a
Erik Raith <raithhtiar@gmail.com>
2020-03-18 21:23:59 -0600

    [feat] Add searchForm class

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
-import './SearchForm.css';
+import React, { Component } from "react";
+import "./SearchForm.css";
 
 class SearchForm extends Component {
   constructor() {
-    super()
-      this.state = {
-        search:''
-      }
+    super();
+    this.state = {
+      search: ""
+    };
   }
-  updateState = (event) => {
-    console.log(event.target.value)
+
+  updateState = event => {
     const updatedState = event.target.value;
-    this.setState({search:updatedState})
+    this.setState({ search: updatedState });
+  };
+
+  searchSubmit = () => {
+    this.props.search(this.state.search)
   }
+
   render() {
     return (
       <React.Fragment>
-      <label for = "search">Search</label>
-      <input id = "search" onChange = {event => this.updateState(event)}/>
+        <label for="search">Search</label>
+        <input id="search" onChange={event => this.updateState(event)} />
+        <button onClick = {event => this.searchSubmit(event)}>Search</button>
       </React.Fragment>
-    )
+    );
   }
 }
 
