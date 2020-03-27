f37c6da745caa96c6ee20f623875000a3475a632
Erik Raith <raithhtiar@gmail.com>
2020-03-27 13:16:58 -0600

    Updates

---
+++ precache-manifest.6d0b9b9ea89e20b49bbf1a78205c13c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b2fdd5902d399db5e8f99d78668e00",
+    "url": "/whats-new/index.html"
+  },
+  {
+    "revision": "f065119c450ce799ef53",
+    "url": "/whats-new/static/css/main.ff6f1128.chunk.css"
+  },
+  {
+    "revision": "c59844115be4ae0562e5",
+    "url": "/whats-new/static/js/2.440ec2f2.chunk.js"
+  },
+  {
+    "revision": "f065119c450ce799ef53",
+    "url": "/whats-new/static/js/main.2a6e613b.chunk.js"
+  },
+  {
+    "revision": "1f94ce0c8e9eb67f44d3",
+    "url": "/whats-new/static/js/runtime-main.2216c931.js"
+  }
+]);
